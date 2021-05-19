ba65503798425a1cf2a95b88232cb2e8394af492
smilejoeeee <watcharapong@builk.com>
2021-05-19 21:21:45 +0700

    Create Page

---
+++ sell_si/lib/screen/AddItem.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class AdditemPage extends StatefulWidget {
+  @override
+  _AdditemPageState createState() => _AdditemPageState();
+}
+
+class _AdditemPageState extends State<AdditemPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
