9748703f2838ec0effd8897a858185cdc86cea84
smilejoeeee <watcharapong@builk.com>
2021-05-21 01:12:11 +0700

    index

--- sell_si/lib/screen/List.dart
+++ sell_si/lib/screen/List.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class ListPage extends StatefulWidget {
   @override
@@ -6,10 +9,49 @@ class ListPage extends StatefulWidget {
 }
 
 class _ListPageState extends State<ListPage> {
+    final auth =  FirebaseAuth.instance;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My Product"),
+      ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection("Products").where('user', isEqualTo :auth.currentUser!.email).snapshots(),
+        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (!snapshot.hasData) {
+            return Center(
+              child: Text("ยังไม่มีข้อมูล"),
+            );
+          }
+          return ListView(
+              children: snapshot.data!.docs.map((product) {
+            return Container(
+              child: ListTile(
+                leading: CircleAvatar(
+                  maxRadius: 30,
+                  minRadius: 30,
+                  child: FittedBox(
+                    child: Text(product["price"]),
+                  ),
+                ),
+                title: Text(product["title"]),
+                subtitle: Text(product["desciption"]),
+                trailing: IconButton(
+                  icon: const Icon(Icons.delete),
+                  tooltip: 'Delete  item : ' + product["title"],
+                  onPressed: () {
+                     ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                        content: Text(
+                            "ลบสินค้า " + product["title"] + "เรียบร้อยแล้ว"),
+                      ));
+                  },
+                ),
+              ),
+            );
+          }).toList());
+        },
+      ),
     );
   }
 }
--- sell_si/lib/screen/index.dart
+++ sell_si/lib/screen/index.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:sell_si/screen/index/body.dart';
@@ -7,7 +8,42 @@ class IndexScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildAppBar(),
-      body: Body(),
+       body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection("Products").snapshots(),
+        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (!snapshot.hasData) {
+            return Center(
+              child: Text("ยังไม่มีข้อมูล"),
+            );
+          }
+          return ListView(
+              children: snapshot.data!.docs.map((product) {
+            return Container(
+              child: ListTile(
+                leading: CircleAvatar(
+                  maxRadius: 30,
+                  minRadius: 30,
+                  child: FittedBox(
+                    child: Text(product["price"]),
+                  ),
+                ),
+                title: Text(product["title"]),
+                subtitle: Text(product["desciption"]),
+                trailing: IconButton(
+                  icon: const Icon(Icons.delete),
+                  tooltip: 'Delete  item : ' + product["title"],
+                  onPressed: () {
+                     ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                        content: Text(
+                            "ลบสินค้า " + product["title"] + "เรียบร้อยแล้ว"),
+                      ));
+                  },
+                ),
+              ),
+            );
+          }).toList());
+        },
+      ),
     );
   }
 
@@ -15,7 +51,7 @@ class IndexScreen extends StatelessWidget {
     return AppBar(
       elevation: 0,
       leading: IconButton(
-        icon: SvgPicture.asset("assets/menu.svg"),
+        icon: Icon(Icons.delete_forever),
         onPressed: () {},
       ),
     );
